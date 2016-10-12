2b91c555e9752b8b5c5143f43f60674376ab4c53
haibin <sunhaibin@goyoo.com>
2016-10-12 20:29:49 +0800

    add the movement

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,10 +3,75 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
+    motto: 'Hi Man',
     userInfo: {},
     movements: [
-      {""},
+        {
+          "position": 1,
+          "name": "俯卧撑",
+          "nameEn": "Push Up",
+          "iconUrl": "http://7xl4qs.com1.z0.glb.clouddn.com/icon_pushup.png",
+          "muscleGroup": "胸肌（胸大肌和胸小肌）、三角肌前束、肱三头肌",
+          "muscleGroupImgList": "http://epaper.dfdaily.com/dfzb/res/1/1/2015-02/25/A12/res07_attpic_brief.jpg",
+          "backgroundImgUrl": "steps_1_10_1.png",
+          "descriptions": "  不同形式的俯卧撑锻炼肌肉的程度不同,但俯卧撑的所有变式都能增强力量、打肌肉。 俯卧撑尤其锻炼胸大肌、三角肌前束和胸小肌,从而最大限度地发展你躯干上的推力肌肉。 此外,俯卧撑还能够锻炼肱三头肌(上臂的主要肌肉)的三个头。",
+          "slogan": "铠甲般的胸肌与钢铁般的肱三头肌"
+        },
+        {
+          "position": 2,
+          "name": "深蹲",
+          "nameEn": "Squats",
+          "iconUrl": "http://7xl4qs.com1.z0.glb.clouddn.com/icon_squat.png",
+          "muscleGroup": "",
+          "muscleGroupImgList": "",
+          "backgroundImgUrl": "steps_2_10_1.png",
+          "descriptions": "",
+          "slogan": "升降机般的大腿"
+        },
+        {
+          "position": 3,
+          "name": "引体向上",
+          "nameEn": "Pull Up",
+          "iconUrl": "http://7xl4qs.com1.z0.glb.clouddn.com/icon_pullup.png",
+          "muscleGroup": "",
+          "muscleGroupImgList": "",
+          "backgroundImgUrl": "steps_3_10_1.png",
+          "descriptions": "",
+          "slogan": "仓门般的背部与大炮般的肱二头肌"
+        },
+        {
+          "position": 4,
+          "name": "举腿",
+          "nameEn": "Leg Raise",
+          "iconUrl": "http://7xl4qs.com1.z0.glb.clouddn.com/icon_legraise.png",
+          "muscleGroup": "",
+          "muscleGroupImgList": "",
+          "backgroundImgUrl": "steps_4_10_2.png",
+          "descriptions": "",
+          "slogan": "魔鬼六块"
+        },
+        {
+          "position": 5,
+          "name": "桥",
+          "nameEn": "Bridge",
+          "iconUrl": "http://7xl4qs.com1.z0.glb.clouddn.com/icon_bridges.png",
+          "muscleGroup": "",
+          "muscleGroupImgList": "",
+          "backgroundImgUrl": "steps_5_10_1.png",
+          "descriptions": "",
+          "slogan": "严阵以待的脊柱"
+        },
+        {
+          "position": 6,
+          "name": "倒立撑",
+          "nameEn": "Hand Stand",
+          "iconUrl": "http://7xl4qs.com1.z0.glb.clouddn.com/icon_handstand.png",
+          "muscleGroup": "",
+          "muscleGroupImgList": "",
+          "backgroundImgUrl": "steps_6_10_1.png",
+          "descriptions": "",
+          "slogan": "健康、强力的肩膀"
+        }
     ]
   },
   //事件处理函数
